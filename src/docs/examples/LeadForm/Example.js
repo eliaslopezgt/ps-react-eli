94d861d963e19621887da1231000163d15fcf659
Elias Lopez Gutierrez <elias.lopez@senioradvisor.com>
2017-09-18 15:23:47 -0500

    tesT

---
+++ src/docs/examples/LeadForm/Example.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import LeadForm from 'ps-react/LeadForm';
+
+/** Standard LeadForm */
+export default function ExampleStandard() {
+    return <LeadForm action="." className="testclass" htmlId="testformId" method="post" name="LeadFormName" />
+}
